d69381c76d70c63c3c8de8f4d16957aff1b97818
Joshua Jimenez <jsjimenez51@gmail.com>
2019-01-08 21:33:03 +0000

    UPDATE: imported in models

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -6,7 +6,15 @@ Module defines  DB storage
 from os import getenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, scoped_session
-from models.base_model import Base
+import models
+from models.base_model import Base, BaseModel
+from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
+
 
 class DBStorage:
     """
