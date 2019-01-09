d63d47823e1fb6435cd1dce541295b1cdf695cd0
Joshua S. Jimenez <Jsjimenez51@gmail.com>
2019-01-09 14:25:37 -0800

    UPDATE: fixes to relationship build for City and State objects

--- models/city.py
+++ models/city.py
@@ -3,6 +3,7 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
+from models.place import Place
 
 class City(BaseModel, Base):
     """This is the class for City
@@ -14,4 +15,6 @@ class City(BaseModel, Base):
     __tablename__ = 'cities'
     name = Column(String(128), nullable=False)
     state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
-    places = relationship("Place", cascade="all, delete-orphan", backref="cities")
+    places = relationship("Place", cascade="all, delete-orphan",
+    backref="cities")
+    state = relationship("State")
