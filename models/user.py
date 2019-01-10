6f00f00e3fd40ce399756603ad405b094f946dfd
Joshua S. Jimenez <Jsjimenez51@gmail.com>
2019-01-09 16:14:20 -0800

    Update: Changes for task 10

--- models/user.py
+++ models/user.py
@@ -3,7 +3,8 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import relationship
-
+from models.places import Place
+from models.review import Review
 
 class User(BaseModel, Base):
     """This is the class for user
