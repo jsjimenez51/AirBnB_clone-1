ab1c5e2d9ad30217e8ba0ba90fc2c6ed7e2fe8c6
jsjimenez51 <jsjimenez51@gmail.com>
2019-01-24 14:42:36 -0800

    ADD: file for compressing folder to tar file

---
+++ 1-pack_web_static.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+'''Fabric script that will generate a .tgz archive from a folder'''
+from fabric.api import local
+from datetime import datetime
+from os.path import exists
+from os import makedirs
+import os
+
+
+def do_pack():
+    '''
+    Generates a .tgz archive
+    Return:
+    path to the archive, else none
+    '''
+    time = datetime.now().strftime("%Y%m%d%H%M%S")
+    print("Packing web_static to versions/web_static_{}.tgz".format(time))
+
+    if not exists('versions'):
+        makedirs('versions')
+
+    pack = local("tar -cvzf versions/webstatic_{}.tgz web_static/"
+                 .format(time))
+
+    size = os.stat("versions/webstatic_{}.tgz".format(time)).st_size
+
+    if pack.succeeded:
+        print("web_static packed: versions/web_static_{} -> {}Bytes"
+              .format(time, size))
+        return "versions/web_static_{}.tgz".format(time)
+    else:
+        return None
