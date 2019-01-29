7d350d150cda3fabb287f0b45d3419fb0ed5c1ed
jsjimenez51 <jsjimenez51@gmail.com>
2019-01-29 12:09:27 -0800

    ADD: file for task 1

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+'''
+starts a Flask web app
+listening on 0.0.0.0, port 5000
+Routes: /: display "Hello HBNB!"
+'''
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hbnb_hello():
+    '''
+    Displays Hello HBNB if successful
+    '''
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def HBNB():
+    '''
+    Displays HBNB if successful
+    '''
+    return 'HBNB'
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
