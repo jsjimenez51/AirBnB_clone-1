f9232d9edf8d421d999806ec51c47bd2b4cce78f
jsjimenez51 <jsjimenez51@gmail.com>
2019-01-30 18:19:55 -0800

    UPDATE: fixed the python text input

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -32,7 +32,7 @@ def c_text(text):
 
 
 @app.route('/python', strict_slashes=False)
-@app.route('/python/(<text>)', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
 def py_text(text='is cool'):
     '''
     Displays python followed by text
