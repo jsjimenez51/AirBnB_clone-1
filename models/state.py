17606670f175ec31e8f4f525c2a45445c3f44390
jsjimenez51 <jsjimenez51@gmail.com>
2019-01-31 15:32:53 -0800

    UPDATE: added import of storage from models to state.py

--- models/state.py
+++ models/state.py
@@ -5,6 +5,7 @@ from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
 from sqlalchemy.orm import relationship
 from models.city import City
 from os import getenv
+from models import storage
 
 class State(BaseModel, Base):
     """This is the class for State
